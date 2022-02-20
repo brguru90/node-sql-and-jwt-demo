3621a2a8ccb8090a4fa8fd9c650b3b0f5c6cdcee
Guruprasad BR <brguru90@gmail.com>
2022-02-20 17:39:35 +0530

    bench

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,12 @@ module.exports = {
             script: "./backend/src/server.js",
             watch: false,
             "ignore_watch": ["production.database.sqlite", "production.database.sqlite-journal"],
+            "max_memory_restart": "2000M",
+            "node_args": [
+                "--max_old_space_size=3000"
+            ],
+            instances: "max",
+            exec_mode: "cluster",
             env_qa: {
                 "PORT": 8888,
                 "NODE_ENV": "testing",
@@ -14,11 +20,7 @@ module.exports = {
                 "PORT": 8080,
                 "NODE_ENV": "production",
                 "JWT_SECRET_KEY": "prod_jwt_key_1234"
-            },
-            "max_memory_restart": "2000M",
-            "node_args": [
-                "--max_old_space_size=3000"
-            ]
+            }
         }
     ]
 }

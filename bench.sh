cfe4ebd94f577cfe5078ca079150556f0ff49e26
Guruprasad BR <brguru90@gmail.com>
2022-02-23 22:01:20 +0530

    .

--- bench.sh
+++ bench.sh
@@ -1,17 +1,17 @@
-SQL_USER="818XX@91177.com"
+SQL_USER="D9MOK@GOLZK.com"
 
 echo "SQL_USER=$SQL_USER"
 
 
-access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoiODE4WFhAOTExNzcuY29tIiwidXVpZCI6IjNjN2YzZDYyLTYyMGYtNGZmNy05M2ZhLWFlNzNhNWU3ZDEwMiJ9LCJ1bmFtZSI6IjgxOFhYQDkxMTc3LmNvbSIsInRva2VuX2lkIjoiM2M3ZjNkNjItNjIwZi00ZmY3LTkzZmEtYWU3M2E1ZTdkMTAyXy9HVkFJMlRBZHRMclBUbmRiT2JZQ3hTU1dzM0VzcVkzTnh1OEdvTWw4WDdma1FwMythcmgwMTQ2WUE3WU1zQWFaa1k9XzE2NDUzNjgzOTI5NjciLCJleHAiOjE2NDUzNzE5OTI5NjcsImlhdCI6MTY0NTM2ODM5Mjk2NywiY3NyZl90b2tlbiI6IlZxU0tLWGw3L0pjVWZzVEVITEZ3SnN5cy93MlEybkFRYzl5UURKUVhrSGZBamVSWTVGb2wxdmNJV3Q1NCtJRXdBM2MrM2tQYVd1VnJKdjBrZ3dpaEpFTDB4aDM0b01hVC9xOSt2UE9rZGwrZ1NUZFpEOTFZZEYvb29MU1FMQzBPTE5IMFhRPT0ifQ.CLs6sbjtv_IpYWKVeL7AkaZ-AOu_QkMf69wEl-CO4UI"
+access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoiRDlNT0tAR09MWksuY29tIiwidXVpZCI6ImExMTE0NzJhLTlhOTktNDQyMS1hNDU0LWUwNjQ3NmYyMWRhMSJ9LCJ1bmFtZSI6IkQ5TU9LQEdPTFpLLmNvbSIsInRva2VuX2lkIjoiYTExMTQ3MmEtOWE5OS00NDIxLWE0NTQtZTA2NDc2ZjIxZGExX3RkL05BbDBUNndIQWZTaUpUaDFFb0FkWFpKQW9tLzlsMUZ4TkFkTllUeGhBbkh5Q3EzMmRYUkhNdmY1TXp0dk5FWWc9XzE2NDU2MzI4MDUzMDYiLCJleHAiOjE2NDU2MzY0MDUzMDYsImlhdCI6MTY0NTYzMjgwNTMwNiwiY3NyZl90b2tlbiI6IkcwbkpvdGoxZ3ZQZkZ2TVZvdWhOd2N6eWl1QmE3ODVoY0xOWS9zV2I0WEhGZUFoN2JiRVBzQzVVVDUycXNHZGpiVnhXQm93K2xHbnBub0FOZTlZaWpkdHROOWdJZm9vcEJPMEpibGFQdFlMQVdBbVh1MDlSZS9BUkVaQmJwL0ZQQTBnb0tnPT0ifQ.wE61dSC1cfJHTgJm23nYKKnMmdHsBk9Gsqk9kJk8PEE"
 
-csrf_token="VqSKKXl7/JcUfsTEHLFwJsys/w2Q2nAQc9yQDJQXkHfAjeRY5Fol1vcIWt54+IEwA3c+3kPaWuVrJv0kgwihJEL0xh34oMaT/q9+vPOkdl+gSTdZD91YdF/ooLSQLC0OLNH0XQ=="
+csrf_token="G0nJotj1gvPfFvMVouhNwczyiuBa785hcLNY/sWb4XHFeAh7bbEPsC5UT52qsGdjbVxWBow+lGnpnoANe9YijdttN9gIfoopBO0JblaPtYLAWAmXu09Re/AREZBbp/FPA0goKg=="
 
 ab -C "access_token=$access_token" -H "csrf_token: $csrf_token" -n 100000 -c 1000 -l "http://localhost:8080/api/login_status/" 
 
 ab -C "access_token=$access_token" -H "csrf_token: $csrf_token" -n 100000 -c 1000 -l "http://localhost:8080/api/user/"
 
-ab -C "access_token=$access_token" -H "csrf_token: $csrf_token" -n 100000 -c 1000 -l "http://localhost:8080/api/user?page=1&limit=1000"
+ab -C "access_token=$access_token" -H "csrf_token: $csrf_token" -n 100000 -c 1000 -l "http://localhost:8080/api/user?page=1&limit=20"
 
-ab -C "access_token=$access_token" -H "csrf_token: $csrf_token" -n 100000 -c 1000 -l "http://localhost:8080/api/user?page=1000&limit=1000"
+ab -C "access_token=$access_token" -H "csrf_token: $csrf_token" -n 100000 -c 1000 -l "http://localhost:8080/api/user?page=1000&limit=20"
 
